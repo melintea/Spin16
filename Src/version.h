65bff99537e076ecac175f65bc05139b6b00d2b9
Aurelian MELINTE <ame01@gmx.net>
2026-05-16 13:11:46 -0400

    version.h

--- Src/version.h
+++ Src/version.h
@@ -6,4 +6,4 @@
  * Tool documentation is available at http://spinroot.com
  */
 
-#define SpinVersion	"Spin Version 6.5.2.x -- 18 September 2025"
+#define SpinVersion	"Spin Version 6.5.2-spin16 -- 18 September 2025"
