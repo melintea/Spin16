cc5fda54dafac7f4ad3ae8b746c488275b9e8eca
Aurelian MELINTE <ame01@gmx.net>
2026-05-19 09:26:32 -0400

    _test_spin16.sh

--- Src/_test_spin16.sh
+++ Src/_test_spin16.sh
@@ -1,33 +1,41 @@
 #~/bin/bash
 
+function runcmd()
+{
+    cmd=$1
+    echo "=== ${cmd} ==="
+    ${cmd}
+    return $?
+}
+
 function runit()
 {
     testf=$2
     spinf=$1
-    printf "\n\n===\n"
+    printf "\n\n\n===\n"
     echo "=== $spinf: $testf ==="
-    echo "==="
+    printf "===\n\n"
     $spinf -V
     
     rm ./pan* ./*.trail ./_spin*
     
-    $spinf -a $testf
+    $spinf -a $testf || exit 1
     gcc -E pan.c -o pan.i
     
     #                       -DCOLLAPSE 
-    gcc -ggdb -DMEMLIM=4096 -DBITSTATE -O2 -DXUSAFE -DSAFETY -DNOCLAIM -w -o pan pan.c || exit 1
-    ./pan -m1000001
+    runcmd "gcc -ggdb -DMEMLIM=4096 -DBITSTATE -O2 -DXUSAFE -DSAFETY -DNOCLAIM -w -o pan pan.c" || exit 
+    runcmd "./pan -m1000001"
     
-    gcc -ggdb -DMEMLIM=8192 -DBITSTATE -O2 -DNP -DNOCLAIM -w -o pan pan.c || exit 1
-    ./pan -m1000001 -l
+    runcmd "gcc -ggdb -DMEMLIM=8192 -DBITSTATE -O2 -DNP -DNOCLAIM -w -o pan pan.c" || exit 
+    runcmd "./pan -m1000001 -l"
     
-    gcc -ggdb -DMEMLIM=8192 -DBITSTATE -O2 -DNFAIR=100 -w -o pan pan.c || exit 1
-    ./pan -m1000001 -a -N p0
-    ./pan -m1000001 -a -f -N p0
-    ./pan -m1000001 -a -N p1
-    ./pan -m1000001 -a -f -N p1
-    #./pan -a -N p2
-    #./pan -a -N p3
+    runcmd "gcc -ggdb -DMEMLIM=8192 -DBITSTATE -O2 -DNFAIR=100 -w -o pan pan.c" || exit 
+    runcmd "./pan -m1000001 -a -N p0"
+    runcmd "./pan -m1000001 -a -f -N p0"
+    runcmd "./pan -m1000001 -a -N p1"
+    runcmd "./pan -m1000001 -a -f -N p1"
+    #runcmd "./pan -a -N p2"
+    #runcmd "./pan -a -N p3"
 
     #rm ./pan* ./*.trail ./_spin*
 }
