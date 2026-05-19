432f719b133eaa9848693c5e43993f63e5c717b0
Aurelian MELINTE <ame01@gmx.net>
2026-05-19 09:00:40 -0400

    limits.h

--- Src/limits.h
+++ Src/limits.h
@@ -22,7 +22,11 @@
 #    error MAX_SPIN_QID <= SHRT_MAX
 #  endif
 #  define MAX_SPIN_MSGID    SHRT_MAX
-#  define MAXQ        MAX_SPIN_QID
+#  if MAX_SPIN_QID > 2500
+#    define MAXQ        MAX_SPIN_QID
+#  else
+#    define MAXQ        2500
+#  endif
 #  define MAXPROC     MAX_SPIN_PID
 /* VECTORSZ in disguise, value must be in clear */
 #  define XVECTORSZ    1048592  /* > (MAXPROC+MAXQ+4)*sizeof(void*) on 64 bits */
